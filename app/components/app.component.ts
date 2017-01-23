a55a18aa5d47ba0fd14935b137e34b2250d687aa
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-01-23 17:20:59 +0300

    re

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,7 +1,9 @@
-import {Component} from 'angular2/core';
-// import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {Component, OnInit} from 'angular2/core';
+import { RouteConfig } from 'angular2/router';
+import { ActivatedRoute } from '@angular/router';
+import {ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
 // import {CalculatorComponent} from "./calculator/calculator.component";
-// import {MainPageComponent} from "./main-page/main-page.component";
+import {MainPageComponent} from "./main-page/main-page.component";
 // import {CalculatorService} from "../services/calculator.service";
 
 @Component({
@@ -10,21 +12,26 @@ import {Component} from 'angular2/core';
     styleUrls: ['./components/app.component.css']
 })
 
-// @RouteConfig([
-//     {
-//         path: '/',
-//         name: 'MainPage',
-//         component: MainPageComponent
-//     },
-//     {
-//         path: '/calculator',
-//         name: 'Calculator',
-//         component: CalculatorComponent
-//     }
-// ])
+@RouteConfig([
+    {
+        path: '/',
+        name: 'MainPage',
+        component: MainPageComponent
+    }
+])
 
 
 
-export class AppComponent {
+export class AppComponent implements OnInit{
     name: string = "Angular 2 on Express";
+    sid: string;
+    private sub: any;
+    
+    constructor(private _router: ActivatedRoute){}
+    
+    ngOnInit() {
+        this.sub = this._router.params.subscribe(params => {
+            this.sid = params.sid;
+        })
+    }
 }

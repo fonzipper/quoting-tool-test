47b18e66596a435c18422a123bc1664a4115f1c4
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-01-24 14:23:57 +0300

    added sid passing

--- app/components/about/about.component.ts
+++ app/components/about/about.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Http, Headers} from '@angular/http'
 
 @Component({
     selector: 'my-about',
@@ -10,8 +11,19 @@ export class AboutComponent {
     name: string = "About Us";
     param: string;
 
-    constructor(private params: ActivatedRoute) {
+    constructor(private params: ActivatedRoute, private http: Http) {
         params.params
             .subscribe((data: { id?: string}) => this.param = data.id);
+    }
+
+    testConnection() {
+      var headers = new Headers();
+      headers.append("Content-type", "text/json");
+      headers.append("Authorization", "OAuth " + this.param);
+      return Promise.resolve(this.http.get("https://rc--box22.cs15.my.salesforce.com/services/apexrest/RelaywareRestAPI", {
+          headers: headers
+      }).map(res => {
+          this.name = res;
+      }));
     }
 }

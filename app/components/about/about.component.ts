91034097271f0484f7be58840b27c49d37ff9a67
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-01-24 17:15:56 +0300

    added request to SF

--- app/components/about/about.component.ts
+++ app/components/about/about.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Http, Headers} from '@angular/http'
+import { Http, Headers, Response} from '@angular/http'
 
 @Component({
     selector: 'my-about',
@@ -10,20 +10,32 @@ import { Http, Headers} from '@angular/http'
 export class AboutComponent {
     name: string = "About Us";
     param: string;
+    result: any;
+    failure: Boolean = false;
 
     constructor(private params: ActivatedRoute, private http: Http) {
         params.params
             .subscribe((data: { id?: string}) => this.param = data.id);
     }
 
+    testConn(){
+        this.testConnection().then(result => result.subscribe(result => {
+          // debugger;
+          this.result = result;
+          this.failure = (result.status == 'failure');
+        }))
+    }
+
     testConnection() {
+      // debugger;
       var headers = new Headers();
       headers.append("Content-type", "text/json");
       headers.append("Authorization", "OAuth " + this.param);
-      return Promise.resolve(this.http.get("https://rc--box22.cs15.my.salesforce.com/services/apexrest/RelaywareRestAPI", {
+      return Promise.resolve(this.http.get("https://rc--box22.cs15.my.salesforce.com/services/apexrest/RelaywareRestAPI?id=006e000000Ctrpz", {
           headers: headers
-      }).map(res => {
-          this.name = res;
+      }).map((res: Response) => {
+          return res.json();
       }));
+      // console.log(rr);
     }
 }

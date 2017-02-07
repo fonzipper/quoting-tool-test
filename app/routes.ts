a8b4da0e9c643a63ef6a06e728e301994dac6726
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-02-07 16:22:41 +0300

    fixed routing

--- app/routes.ts
+++ app/routes.ts
@@ -5,8 +5,8 @@ import { AboutComponent } from './components/about/about.component';
 
 export const routes: Route[] = [
     { path: '', redirectTo: '/home', pathMatch: 'full'},
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent }
+    { path: '/home', component: HomeComponent },
+    { path: '/about', component: AboutComponent }
 ];
 
-export const routing = RouterModule.forRoot(routes, { useHash: false });
+export const routing = RouterModule.forRoot(routes, { useHash: true });

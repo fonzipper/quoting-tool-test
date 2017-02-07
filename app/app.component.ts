c99a9cf8ca7a108fdfaadc00526fb04654772323
Nikita Sergienko <nsergienko@aquivalabs.com>
2017-02-07 14:17:06 +0300

    creating empty app for Billing Project

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,18 +1,10 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'my-app',
-    styles: [`h1 {
-	color: white;
-	background: darkgray;
-	padding: 20px;
-}
-`],
-    template: `
-<h1>My First {{name}} app</h1>
-<router-outlet></router-outlet>
-
-<a [routerLink]="['/']">Home</a> | <a [routerLink]="['/about/', { id: 2 }]">About</a>`,
+    styleUrls: ['app.component.css'],
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {
     name: string = "Angular 2 on Express";
--- app/routes.ts
+++ app/routes.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 
 export const routes: Route[] = [
-    { path: '', component: HomeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent }
 ];
 
-export const routing = RouterModule.forRoot(routes, { useHash: true });
+export const routing = RouterModule.forRoot(routes, { useHash: false });
